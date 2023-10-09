ca7129343b141a3f240f455ec4a0c1e0cc27da70
Elías Ferreira <eliosferre3@hotmail.com>
2023-10-09 17:56:59 -0300

    Add CORS allowance

--- index.js
+++ index.js
@@ -16,6 +16,17 @@ connectToDb((err) => {
     }
 });
 
+/* Enable CORS */
+app.use((req, res, next) => {
+    res.header(
+        "Access-Control-Allow-Origin",
+        "http://localhost:5173, https://eliasferreira.netlify.app/"
+    );
+    res.header("Access-Control-Allow-Methods", "GET");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
 /* Routes */
 app.get("/projects", (req, res) => {
     const projects = [];
