ef1265d0cb027b0d8a527e9cb3c70875712ecafa
Elías Ferreira <eliosferre3@hotmail.com>
2024-05-31 01:52:51 -0300

    Update vercel config

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { connectToDb, getDb } from "./db.js";
 
 /* Initialize express app */
 const app = express();
 const PORT = process.env.PORT || 3000;
+const APP_PATH = process.env.APP_PATH || "*";
 
 /* Database connection */
 let db;
@@ -17,12 +19,7 @@ connectToDb((err) => {
 });
 
 /* Enable CORS */
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-});
+app.use(cors({ origin: APP_PATH }));
 
 /* Home route */
 app.get("/", (req, res) => {
